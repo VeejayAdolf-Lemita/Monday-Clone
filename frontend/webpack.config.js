7d14ece7a01aacc3d30871f37721dfe7a9c77193
VeejayAdolf-Lemita <102731011+VeejayAdolf-Lemita@users.noreply.github.com>
2023-06-09 19:27:21 +0800

    error build

---
+++ frontend/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './build',
+    port: 3000,
+    allowedHosts: ['*'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+        loader: 'file-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
